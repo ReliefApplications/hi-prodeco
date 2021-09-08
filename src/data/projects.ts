d2d0fc99fe5f436fea4e363ce9c766c636a515cf
adrianmujica <adrian@reliefapplications.org>
2021-09-08 08:58:43 +0100

    added description to the project pictures

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -128,19 +128,23 @@ export const Projects: Project[] = [
       }),
       new Gallery({
         url: 'assets/projects/assistance_aux_victimes/2.SECADEV-Djedouboum_mécanicien3.jpg',
-        author: 'Debora Edith Ngaba'
+        author: 'Debora Edith Ngaba',
+        description: 'debora.fada_2'
       }),
       new Gallery({
         url: 'assets/projects/assistance_aux_victimes/3.SECADEV-Rachel_la_commercante 2.jpg',
-        author: 'Debora Edith Ngaba'
+        author: 'Debora Edith Ngaba',
+        description: 'debora.fada_3'
       }),
       new Gallery({
         url: 'assets/projects/assistance_aux_victimes/4. SECADEV-Rosine_la_souriante_1.jpg',
-        author: 'Debora Edith Ngaba'
+        author: 'Debora Edith Ngaba',
+        description: 'debora.fada_4'
       }),
       new Gallery({
         url: 'assets/projects/assistance_aux_victimes/5. SECADEV-Neloumta_la_reine2.jpg',
-        author: 'Debora Edith Ngaba'
+        author: 'Debora Edith Ngaba',
+        description: 'debora.fada_5'
       }),
       new Gallery({
         url: 'assets/projects/assistance_aux_victimes/PRODECO_assistance_aux_victimes_NDjamena-3.jpg',
@@ -159,7 +163,8 @@ export const Projects: Project[] = [
       }),
       new Gallery({
         url: 'assets/projects/assistance_aux_victimes/1.SECADEV-Anatole_aide_prothésiste.jpg',
-        author: 'Debora Edith Ngaba'
+        author: 'Debora Edith Ngaba',
+        description: 'debora.fada_1'
       })
     ]),
 
@@ -171,19 +176,23 @@ export const Projects: Project[] = [
     [
       new Gallery({
         url: 'assets/projects/insertion_socio_economique/1 . Activité HI - Achat de mouton par les béneficiaires.jpg',
-        author: 'Djérabé Ndigngar'
+        author: 'Djérabé Ndigngar',
+        description: 'djerabe.fada_1_5'
       }),
       new Gallery({
         url: 'assets/projects/insertion_socio_economique/5. Activité HI - Programme ISE - Brahim accompagne sa maman qui est bénéficaire du programme ISE.jpg',
-        author: 'Djérabé Ndigngar'
+        author: 'Djérabé Ndigngar',
+        description: 'djerabe.fada_1_5'
       }),
       new Gallery({
         url: 'assets/projects/insertion_socio_economique/8. Activité HI - Programme ISE - Aché - Commerçante au marché fada-.jpg',
-        author: 'Djérabé Ndigngar'
+        author: 'Djérabé Ndigngar',
+        description: 'djerabe.fada_8'
       }),
       new Gallery({
         url: 'assets/projects/insertion_socio_economique/16. Activité HI - Programme ISE - Recensement d_un bénéficiaire du programme ISE 1.jpg',
-        author: 'Djérabé Ndigngar'
+        author: 'Djérabé Ndigngar',
+        description: 'djerabe.fada_16'
       }),
       new Gallery({
         url: 'assets/projects/insertion_socio_economique/insertion socio-economique_portrait bénéficaire_Fada-5.jpg',
@@ -227,7 +236,8 @@ export const Projects: Project[] = [
       }),
       new Gallery({
         url: 'assets/projects/insertion_socio_economique/PRODECO_insertion socio-économique_portrait bénéficaire_Fada-3.jpg',
-        author: 'Gwenn Dubourthoumieu'
+        author: 'Gwenn Dubourthoumieu',
+        description: 'gwenn.beneficiaire_fada_3'
       }),
       new Gallery({
         url: 'assets/projects/insertion_socio_economique/PRODECO_insertion_socio-economique_portrait_beneficaire_Fada-6.jpg',
@@ -299,7 +309,8 @@ export const Projects: Project[] = [
       }),
       new Gallery({
         url: 'assets/projects/appui_institutionnel/17. PRODECO_renforcement institutionnel_NDjamena-16.jpg',
-        author: 'Djérabé Ndigngar'
+        author: 'Djérabé Ndigngar',
+        description: 'djerabe.fada_17'
       })
     ]
   ),
